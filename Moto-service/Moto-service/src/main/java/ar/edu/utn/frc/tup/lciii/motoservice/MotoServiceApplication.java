f43a4b3dc15c661d26001b2e9f5f0397b5e07a01
404975-Zabala-joaquin <404975@tecnicatura.frc.utn.edu.ar>
2024-09-02 10:42:06 -0300

    eureka

--- Moto-service/Moto-service/src/main/java/ar/edu/utn/frc/tup/lciii/motoservice/MotoServiceApplication.java
+++ Moto-service/Moto-service/src/main/java/ar/edu/utn/frc/tup/lciii/motoservice/MotoServiceApplication.java
@@ -1,7 +1,10 @@
 package ar.edu.utn.frc.tup.lciii.motoservice;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class MotoServiceApplication {
